f06114d6a2c15ec7294ec7042fbb3036532cd842
EmilRingtved <e.ringtved@gmail.com>
2022-11-22 11:47:37 +0100

    Formatting, attaching leds after exiting sleepmode

--- app.c
+++ app.c
@@ -25,8 +25,8 @@
 #include <std_msgs/msg/float32.h>
 #include <geometry_msgs/msg/twist.h>
 
-//macros functions, Constrain from arduino.h and RCCHECK from microros examples
-#define constrain(amt, low, high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+// macros functions, Constrain from arduino.h and RCCHECK from microros examples
+#define constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 #define RCCHECK(fn)                                                                      \
     {                                                                                    \
         rcl_ret_t temp_rc = fn;                                                          \
@@ -79,48 +79,44 @@ std_msgs__msg__Float32 battery_msg;
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Twist msg;
 
-void setup();
+void GPIOsetup();
+void PWMsetup();
 void motorControl(float vel, float a);
 float batteryVoltage();
 void cmd_vel_callback(const void *msgin);
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time);
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time);
 void microRosTask();
 
-void setup(){
-    //set internals
+void GPIOsetup()
+{
+    // Setup function for setting the GPIO settings
+    // set internals
     gpio_set_direction(stayOn, GPIO_MODE_OUTPUT);
     gpio_set_direction(vSens, GPIO_MODE_INPUT);
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(vSens, ADC_ATTEN_DB_0);
     gpio_set_level(stayOn, 1);
 
-    //set all pwm outouts
+    // set all pwm outouts
     gpio_set_direction(PinL1, GPIO_MODE_OUTPUT);
     gpio_set_direction(PinL2, GPIO_MODE_OUTPUT);
     gpio_set_direction(PinR1, GPIO_MODE_OUTPUT);
     gpio_set_direction(PinR2, GPIO_MODE_OUTPUT);
-    
-    //exit sleepmode
-    gpio_set_level(PinL1,1);
-    gpio_set_level(PinL2,1);
-    gpio_set_level(PinR1,1);
-    gpio_set_level(PinR2,1);
-    vTaskDelay(1 / portTICK_RATE_MS); //wait for 1ms
-    //reset gpio mode
-    gpio_reset_pin(PinL1);
-    gpio_reset_pin(PinL2);
-    gpio_reset_pin(PinR1);
-    gpio_reset_pin(PinR2);
+}
+void PWMsetup()
+{
+    // Setup function for setting the pwm channels, has to be called when
+    // exiting sleepmode
     // pwm timer struct set to match motor drivers.
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = PWM_resolution,
         .freq_hz = PWM_FREQ,
         .speed_mode = PWM_spdMode,
         .timer_num = PWM_timer,
-        .clk_cfg = LEDC_AUTO_CLK
-    };
+        .clk_cfg = LEDC_AUTO_CLK};
     ledc_timer_config(&ledc_timer);
 
+    //filling the led_channel_config struct with the defined PWM parameters
     ledc_channel_config_t ledc_channel[4] = {
         {.channel = PWM_L1,
          .duty = 0,
@@ -155,114 +151,139 @@ void setup(){
         ledc_channel_config(&ledc_channel[i]);
     }
 }
-    void microRosTask(){
-        
-        rcl_allocator_t allocator = rcl_get_default_allocator();
 
-        // microRos INIT options
-        rclc_support_t support;
-        RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+void microRosTask()
+{
+    //Main task, running the microros system and handles subscriber information.
 
-        // create node
-        rcl_node_t node;
-        RCCHECK(rclc_node_init_default(&node, "LanciersBot", "", &support));
+    rcl_allocator_t allocator = rcl_get_default_allocator();
 
-        // create subscriber, expecting msg in format of /cmd_vel, connect to topic /cmd_vel
-        // initiated to connect with best effort for faster communication
-        RCCHECK(rclc_subscription_init_best_effort(
-            &subscriber,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
-            "/cmd_vel"));
-        // create publisher for returning batteryVoltage
-        RCCHECK(rclc_publisher_init_default(
-            &publisher,
-            &node,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-            "/batt_volt"));
+    // microRos INIT options
+    rclc_support_t support;
+    RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-        // Create timer.
-        rcl_timer_t timer;
-        RCCHECK(rclc_timer_init_default(
-            &timer,
-            &support,
-            RCL_MS_TO_NS(timer_timeout),
-            timer_callback));
+    // create node
+    rcl_node_t node;
+    RCCHECK(rclc_node_init_default(&node, "LanciersBot", "", &support));
 
-        // Create Executor.
-        rclc_executor_t executor;
-        RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
-        RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &cmd_vel_callback, ON_NEW_DATA));
-        RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    // create subscriber, expecting msg in format of /cmd_vel, connect to topic /cmd_vel
+    // initiated to connect with best effort for faster communication
+    RCCHECK(rclc_subscription_init_best_effort(
+        &subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+        "/cmd_vel"));
+    // create publisher for returning batteryVoltage
+    RCCHECK(rclc_publisher_init_default(
+        &publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+        "/batt_volt"));
 
-        battery_msg.data = 0;
+    // Create timer.
+    rcl_timer_t timer;
+    RCCHECK(rclc_timer_init_default(
+        &timer,
+        &support,
+        RCL_MS_TO_NS(timer_timeout),
+        timer_callback));
 
-        while (1)
-        {
-            rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
-            usleep(10*1000);
-        }
-        // free mem
-        RCCHECK(rcl_subscription_fini(&subscriber, &node));
-        RCCHECK(rcl_publisher_fini(&publisher, &node));
-        RCCHECK(rcl_node_fini(&node));
+    // Create Executor.
+    rclc_executor_t executor;
+    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+    RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &cmd_vel_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-        vTaskDelete(NULL);
+    battery_msg.data = 0;
+
+    while (1)
+    {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
+        usleep(10 * 1000);
     }
+    // free mem
+    RCCHECK(rcl_subscription_fini(&subscriber, &node));
+    RCCHECK(rcl_publisher_fini(&publisher, &node));
+    RCCHECK(rcl_node_fini(&node));
 
-    void cmd_vel_callback(const void *msgin){
-        const geometry_msgs__msg__Twist *msg = (const geometry_msgs__msg__Twist *)msgin;
-        printf("Message received: %f %f\n", msg->linear.x, msg->angular.z);
-    }
+    vTaskDelete(NULL);
+}
 
-    void timer_callback(rcl_timer_t * timer, int64_t last_call_time){
-        if (timer == NULL)
-        {
-             //send battery data back
-            battery_msg.data = batteryVoltage();
-            RCSOFTCHECK(rcl_publish(&publisher, &battery_msg, NULL));
-            printf("Sent: %f\n", battery_msg.data);
-            return;
-        }
-            //run motor,
-        else motorControl(msg.linear.x,msg.angular.z);
-    }
-    // Calculates the current battery voltage
-    float batteryVoltage(){
-        float vMeas = adc1_get_raw(ADC1_CHANNEL_5) * 3.3 / 4096;    // get voltage from adc
-        float vBatt = vMeas * (100 * 10e3 + 22 * 10e3) / 22 * 10e3; // calculate battery voltage from voltagedivider circuit
-        return vBatt;
-    }
-    void motorControl(float vel, float a){
-        /* this is code for steering the robot
-         * it calculates what pwm values to output, but not for how long
-         * vel is the linear velocity and is a value between -1 and 1, with 1 being top speed forward.
-         * a is the angle and is a value between -1 and 1. right is -1 and left is 1
-         */
-        // takes input of linear velocity and angle 
-        // all of this code has been taken from github.com/Reinbert/ros_esp32cam_diffdrive
-        float norm_vel = constrain(vel, -1, 1);
-        float norm_a = constrain(a, -1, 1);
+void cmd_vel_callback(const void *msgin)
+{
+    const geometry_msgs__msg__Twist *msg = (const geometry_msgs__msg__Twist *)msgin;
+    printf("Message received: %f %f\n", msg->linear.x, msg->angular.z);
+}
 
-        // calculate wheel velocities
-        float lVel = (norm_vel - norm_a) / 2.0f; // taken from github - use wheel distance/size??
-        float rVel = (norm_vel + norm_a) / 2.0f;
-
-        // map velocity to pwm
-        uint16_t pwmLeft  = (uint16_t)((fabs(lVel)) * (PWM_max - PWM_min) / (1) + PWM_min);
-        uint16_t pwmRight = (uint16_t)((fabs(rVel)) * (PWM_max - PWM_min) / (1) + PWM_min);
-        
-        ledc_set_duty(PWM_spdMode, PWM_L1, pwmLeft * (lVel > 0));
-        ledc_set_duty(PWM_spdMode, PWM_L2, pwmLeft * (lVel < 0));
-        ledc_set_duty(PWM_spdMode, PWM_R1, pwmRight * (rVel > 0));
-        ledc_set_duty(PWM_spdMode, PWM_R2, pwmRight * (rVel < 0));
-
-        ledc_update_duty(PWM_spdMode, PWM_L1);
-        ledc_update_duty(PWM_spdMode, PWM_L2);
-        ledc_update_duty(PWM_spdMode, PWM_R1);
-        ledc_update_duty(PWM_spdMode, PWM_R2);
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    if (timer == NULL)
+    {
+        // send battery data back
+        battery_msg.data = batteryVoltage();
+        RCSOFTCHECK(rcl_publish(&publisher, &battery_msg, NULL));
+        printf("Sent: %f\n", battery_msg.data);
+        return;
     }
-    void appMain(){
-        setup();
-        microRosTask();
-    }
+    // run motor,
+    else
+        motorControl(msg.linear.x, msg.angular.z);
+}
+// Calculates the current battery voltage
+float batteryVoltage()
+{
+    float vMeas = adc1_get_raw(ADC1_CHANNEL_5) * 3.3 / 4096;    // get voltage from adc
+    float vBatt = vMeas * (100 * 10e3 + 22 * 10e3) / 22 * 10e3; // calculate battery voltage from voltagedivider circuit
+    return vBatt;
+}
+void exitSleepMode(void)
+{
+    // exit sleepmode
+    gpio_set_level(PinL1, 1);
+    gpio_set_level(PinL2, 1);
+    gpio_set_level(PinR1, 1);
+    gpio_set_level(PinR2, 1);
+    // wait for 1ms for motor to enable
+    vTaskDelay(1 / portTICK_RATE_MS);
+
+    // reattach pwm pins
+    PWMsetup();
+}
+void motorControl(float vel, float a)
+{
+    /* this is code for steering the robot
+     * it calculates what pwm values to output, but not for how long
+     * vel is the linear velocity and is a value between -1 and 1, with 1 being top speed forward.
+     * a is the angle and is a value between -1 and 1. right is -1 and left is 1
+     */
+    // takes input of linear velocity and angle
+    // all of this code has been taken from github.com/Reinbert/ros_esp32cam_diffdrive
+    float norm_vel = constrain(vel, -1, 1);
+    float norm_a = constrain(a, -1, 1);
+
+    // calculate wheel velocities
+    float lVel = (norm_vel - norm_a) / 2.0f; // taken from github - use wheel distance/size??
+    float rVel = (norm_vel + norm_a) / 2.0f;
+
+    // map velocity to pwm
+    uint16_t pwmLeft = (uint16_t)((fabs(lVel)) * (PWM_max - PWM_min) / (1) + PWM_min);
+    uint16_t pwmRight = (uint16_t)((fabs(rVel)) * (PWM_max - PWM_min) / (1) + PWM_min);
+
+    exitSleepMode();
+
+    ledc_set_duty(PWM_spdMode, PWM_L1, pwmLeft * (lVel > 0));
+    ledc_set_duty(PWM_spdMode, PWM_L2, pwmLeft * (lVel < 0));
+    ledc_set_duty(PWM_spdMode, PWM_R1, pwmRight * (rVel > 0));
+    ledc_set_duty(PWM_spdMode, PWM_R2, pwmRight * (rVel < 0));
+
+    ledc_update_duty(PWM_spdMode, PWM_L1);
+    ledc_update_duty(PWM_spdMode, PWM_L2);
+    ledc_update_duty(PWM_spdMode, PWM_R1);
+    ledc_update_duty(PWM_spdMode, PWM_R2);
+}
+void appMain()
+{
+    GPIOsetup();
+    PWMsetup();
+    microRosTask();
+}
